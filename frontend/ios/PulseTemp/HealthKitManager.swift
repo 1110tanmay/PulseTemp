653bb886c799c686c778b604fb38ffd8d585502b
Tanmay Shelar <1110tanmay@gmail.com>
2025-03-28 22:40:41 -0700

    Updated the UI, added HealthkitManager.swift to manage healthkit permissions.

---
+++ frontend/ios/PulseTemp/HealthKitManager.swift
@@ -0,0 +1,18 @@
+//
+//  HealthKitManager.swift
+//  PulseTemp
+//
+//  Created by Tanmay Shelar on 3/27/25.
+//
+
+import SwiftUI
+
+struct HealthKitManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthKitManager()
+}
